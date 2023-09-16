38dd583648fac29941c748e30c92cde2a78f0bad
Hrishikesh Choudhari <me@rishi.xyz>
2023-09-17 00:05:31 +0100

    Added comments

--- lib/vectora_web/live/business_model_canvas_live/form_component.ex
+++ lib/vectora_web/live/business_model_canvas_live/form_component.ex
@@ -1,8 +1,14 @@
+# This module represents a LiveComponent for the BusinessModelCanvas form in the VectoraWeb application.
+# It allows users to create or edit BusinessModelCanvas records using a live interactive form.
 defmodule VectoraWeb.BusinessModelCanvasLive.FormComponent do
+  # Use the LiveComponent functionalities provided by the VectoraWeb framework.
   use VectoraWeb, :live_component
 
+  # Alias the module for easier referencing.
   alias Vectora.Tools
 
+  # Implement the render callback to define the HTML structure for this LiveComponent.
+  # This renders the BusinessModelCanvas form with the provided data in the assigns.
   @impl true
   def render(assigns) do
     ~H"""
@@ -29,8 +35,11 @@ defmodule VectoraWeb.BusinessModelCanvasLive.FormComponent do
     """
   end
 
+  # Update callback is called whenever the LiveComponent is mounted or updated with new data.
+  # It initializes or updates the form state based on the provided business_model_canvas data.
   @impl true
   def update(%{business_model_canvas: business_model_canvas} = assigns, socket) do
+    # Create or update the changeset for the provided business_model_canvas.
     changeset = Tools.change_business_model_canvas(business_model_canvas)
 
     {:ok,
@@ -39,6 +48,8 @@ defmodule VectoraWeb.BusinessModelCanvasLive.FormComponent do
      |> assign_form(changeset)}
   end
 
+  # Handle the "validate" event triggered when changes are made to the form.
+  # It validates the data and updates the form state accordingly.
   @impl true
   def handle_event("validate", %{"business_model_canvas" => business_model_canvas_params}, socket) do
     changeset =
@@ -49,10 +60,15 @@ defmodule VectoraWeb.BusinessModelCanvasLive.FormComponent do
     {:noreply, assign_form(socket, changeset)}
   end
 
+  # Handle the "save" event triggered when the form is submitted.
+  # Based on the action (either :edit or :new), it either updates an existing record or creates a new one.
   def handle_event("save", %{"business_model_canvas" => business_model_canvas_params}, socket) do
     save_business_model_canvas(socket, socket.assigns.action, business_model_canvas_params)
   end
 
+  # Private helper function to save changes for an existing BusinessModelCanvas.
+  # On success, it notifies the parent process and updates the UI.
+  # On failure, it updates the form state with the error data.
   defp save_business_model_canvas(socket, :edit, business_model_canvas_params) do
     case Tools.update_business_model_canvas(socket.assigns.business_model_canvas, business_model_canvas_params) do
       {:ok, business_model_canvas} ->
@@ -68,6 +84,9 @@ defmodule VectoraWeb.BusinessModelCanvasLive.FormComponent do
     end
   end
 
+  # Private helper function to create a new BusinessModelCanvas.
+  # On success, it notifies the parent process and updates the UI.
+  # On failure, it updates the form state with the error data.
   defp save_business_model_canvas(socket, :new, business_model_canvas_params) do
     case Tools.create_business_model_canvas(business_model_canvas_params) do
       {:ok, business_model_canvas} ->
@@ -83,9 +102,13 @@ defmodule VectoraWeb.BusinessModelCanvasLive.FormComponent do
     end
   end
 
+  # Private helper function to assign the changeset to the form.
+  # It converts the changeset to a form structure that can be used in the render function.
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do
     assign(socket, :form, to_form(changeset))
   end
 
+  # Private helper function to send a notification to the parent process.
+  # This is used to notify the parent about successful operations like saving a BusinessModelCanvas.
   defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
 end
